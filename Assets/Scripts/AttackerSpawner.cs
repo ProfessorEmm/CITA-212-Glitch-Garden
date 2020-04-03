f95546e8ebc0813f61ef6289a7bcd9c59221a73c
Richard Marcoux <marcourp@morrisville.edu>
2020-04-03 18:55:08 -0400

    S7 L132 - Spawn Attackers Using Coroutine

---
+++ Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerSpawner : MonoBehaviour
+{
+
+    [SerializeField] float fltMinSpawnDelay = 2f;
+    [SerializeField] float fltMaxSpawnDelay = 5f;
+    [SerializeField] Attacker attackerPrefab;
+
+    bool spawn = true;
+
+    IEnumerator Start()
+    {
+        while (spawn)
+        { 
+            // delay before spawning
+            yield return new WaitForSeconds(UnityEngine.Random.Range(fltMinSpawnDelay, fltMaxSpawnDelay));
+            SpawnAttacker();
+        }
+    }
+
+    private void SpawnAttacker()
+    {
+        Instantiate(attackerPrefab, transform.position, transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
