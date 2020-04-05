cb5345050ccce5c3e9fa62caf4fb929777ece1c3
Richard Marcoux <marcourp@morrisville.edu>
2020-04-04 20:43:50 -0400

    S7 L136 - Make Projectile Move

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] float fltSpeed = 1f;
+    
+    void Update()
+    {
+        transform.Translate(Vector2.right * fltSpeed * Time.deltaTime);
+    }
+}
