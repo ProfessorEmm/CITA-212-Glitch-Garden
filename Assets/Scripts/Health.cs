d1a80f39c6648492ba01b8f8302562daff9614d8
Richard Marcoux <marcourp@morrisville.edu>
2020-04-05 15:48:18 -0400

    S7 L138 - Implementing Health & Damage

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] float fltHealth = 100f;
+
+    public void DealDamage(float fltDamage)
+    {
+        fltHealth -= fltDamage;
+        if (fltHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+    
+} // class Health
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     [SerializeField] float fltSpeed = 1f;
+    [SerializeField] float fltDamage = 50f;
     
     void Update()
     {
         transform.Translate(Vector2.right * fltSpeed * Time.deltaTime);
+    } // Update()
+
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        var health = otherCollider.GetComponent<Health>();
+        health.DealDamage(fltDamage);
     }
 }
