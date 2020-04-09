e13123061804b6bbc561e1dec570fa96a8d4a1dc
Richard Marcoux <marcourp@morrisville.edu>
2020-04-09 13:44:17 -0400

    S7 L142 - Snap Defenders to Grid

--- Assets/Scripts/DefenderSpawner.cs
+++ Assets/Scripts/DefenderSpawner.cs
@@ -18,15 +18,27 @@ public class DefenderSpawner : MonoBehaviour
         Vector2 V2clickPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
         // convert from a screen to a World coordinate
         Vector2 V2worldPos = Camera.main.ScreenToWorldPoint(V2clickPos);
-        return V2worldPos;
+        // obtain world (grid) values (as integer)
+        Vector2 V2gridPos = SnapToGrid(V2worldPos);
+        return V2gridPos;
     } // GetSquareClicked()
 
-    private void SpawnDefender(Vector2 V2worldPos)
+    private Vector2 SnapToGrid(Vector2 V2rawWorldPos)
+    {
+        // want to round x and y values to an integer value
+        // obtain x and y values as float
+        float fltNewX = Mathf.RoundToInt(V2rawWorldPos.x);
+        float fltNewY = Mathf.RoundToInt(V2rawWorldPos.y);
+        return new Vector2(fltNewX, fltNewY);
+
+    } // SnapToGrid()
+    private void SpawnDefender(Vector2 V2roundedPos)
     {
         // instantiate a defender with NO rotation 
-        GameObject GOnewDefender = 
-            Instantiate(GOdefender, V2worldPos, 
+        GameObject GOnewDefender =
+            Instantiate(GOdefender, V2roundedPos, 
             Quaternion.identity) as GameObject;
+        Debug.Log(V2roundedPos);
     } // SpawnDefender()
 
 } // class DefenderSapwner
