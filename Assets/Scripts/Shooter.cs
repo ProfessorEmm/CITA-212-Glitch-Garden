72772fe2c7403ae7d70ca4e1cc55ec8c8cfa849e
Richard Marcoux <marcourp@morrisville.edu>
2020-04-16 15:14:45 -0400

    S7 L152 - Attacker in my Lane

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -5,6 +5,58 @@ using UnityEngine;
 public class Shooter : MonoBehaviour
 {
     [SerializeField] GameObject projectile, gun;
+    AttackerSpawner myLaneSpawner;
+
+    private void Start()
+    {
+        SetLaneSpawner();
+    }
+    private void Update()
+    {
+        if (IsAttackerInLane())
+        {
+            Debug.Log("shoot");
+            // change animation state to shooting
+        }
+        else
+        {
+           // Debug.Log("sit and wait");
+            // change animation state to idle
+        }
+        
+    } // Update()
+
+    private void SetLaneSpawner()
+    {
+        // array to store the AttackerSpawners
+        AttackerSpawner[] attackerSpawners = FindObjectsOfType<AttackerSpawner>();
+
+        foreach (AttackerSpawner spawner in attackerSpawners)
+        {
+            // is an attacker in my lane
+            bool boolIsCloseEnough =
+                // use the absolute value of the difference
+                (Mathf.Abs(spawner.transform.position.y - transform.position.y) 
+                <= Mathf.Epsilon);
+            if (boolIsCloseEnough)
+            {
+                myLaneSpawner = spawner;
+            }
+        } // foreach
+
+    } // SetLaneSpawner()
+
+    private bool IsAttackerInLane()
+    {
+        if (myLaneSpawner.transform.childCount <= 0)
+        {
+            return false; // no attacker in our lane
+        }
+        else
+        {
+            return true; // there is an attacker in our lane
+        }
+    } // IsAttackerInLane()
 
     public void Fire()
     {
