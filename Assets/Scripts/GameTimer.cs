469ff68c1fca45f93afe88cd5833c08bc286807b
Richard Marcoux <marcourp@morrisville.edu>
2020-04-28 13:37:29 -0400

    S7 L162 - Create a Level Timer

---
+++ Assets/Scripts/GameTimer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour
+{
+    // add a tooltip to our level timer
+    [Tooltip("Level Timer in SECONDS")]
+    [SerializeField] float fltLevelTime = 10;   
+
+    
+    void Update()
+    {
+        // track time on our slider
+        GetComponent<Slider>().value = Time.timeSinceLevelLoad / fltLevelTime;
+
+        bool boolTimerFinished = (Time.timeSinceLevelLoad >= fltLevelTime);
+        if (boolTimerFinished)
+        {
+            Debug.Log("level timer expired.");
+        }
+    }
+} // class GameTimer
