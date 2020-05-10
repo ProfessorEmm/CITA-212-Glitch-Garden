9ff1b6553f17ffb3cc96fe58089de95ecdb9b6e4
Richard Marcoux <marcourp@morrisville.edu>
2020-05-10 14:19:14 -0400

    S7 L169 - Set & Save Music Volume

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    AudioSource audioSource;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+        audioSource = GetComponent<AudioSource>();
+        // change the volume
+        audioSource.volume = PlayerPrefsController.GetMasterVolume();
+
+    } // Start()
+
+    public void SetVolume(float fltVolume)
+    {
+        audioSource.volume = fltVolume;
+    } // SetVolume()
+
+} // class MusicPlayer
---
+++ Assets/Scripts/OptionsController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsController : MonoBehaviour
+{
+    [SerializeField] Slider volumeSlider;
+    [SerializeField] float fltDefaultVolume = 0.8f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        volumeSlider.value = PlayerPrefsController.GetMasterVolume();
+    } // Start()
+
+    // Update is called once per frame
+    void Update()
+    {
+        // create a MusicPlayer
+        var musicPlayer = FindObjectOfType<MusicPlayer>();
+
+        if (musicPlayer)
+        {
+            // last set value
+            musicPlayer.SetVolume(volumeSlider.value);
+        }
+        else
+        {
+            Debug.LogWarning("No music player found...did you start from Splash screen?");
+        }
+    } // Update()
+
+    public void SaveAndExit()
+    {
+        PlayerPrefsController.SetMasterVolume(volumeSlider.value);
+        FindObjectOfType<LevelLoader>().LoadMainMenu();
+
+    } // SaveAndExit()
+
+    public void SetDefaults()
+    {
+        volumeSlider.value = fltDefaultVolume;
+
+    } // SetDefaults()
+
+
+} // class OptionsController
